c8750c4bac3c944800429a264ae24f175a4ac838
조성원 <tjddnjsjo@kakao.com>
2023-09-14 08:34:09 +0900

    Initial Commit

---
+++ computer graphics/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  computer graphics
+//
+//  Created by Wontory on 9/14/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
