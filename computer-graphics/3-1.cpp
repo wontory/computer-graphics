71d21ce30e03ec8e44fb73c789cd54b23d55bb8a
조성원 <tjddnjsjo@kakao.com>
2023-11-14 14:06:04 +0900

    핵심부 주석 추가

--- computer-graphics/3-1.cpp
+++ computer-graphics/3-1.cpp
@@ -6,25 +6,25 @@
 #include <cmath>
 #include <GL/glut.h>
 
-struct Vertex {
-    float x, y, z;
+struct Vertex {         // 정점 좌표를 저장하는 구조체
+    float x, y, z;      // x, y, z
 };
 
-enum FaceType {
-    TRIANGLE,
-    SQUARE,
-    UNKNOWN
+enum FaceType {     // 면 유형을 저장하는 열거형
+    TRIANGLE,       // 삼각형
+    SQUARE,         // 사각형
+    UNKNOWN         // 알 수 없는 형태
 };
 
-struct Face {
-    std::vector<int> vertices;
-    FaceType type;
+struct Face {                       // 면 정보를 저장하는 구조체
+    std::vector<int> vertices;      // 정점의 index를 저장하는 벡터
+    FaceType type;                  // 면의 형태
 };
 
-std::vector<Vertex> vertices;
-std::vector<Face> faces;
+std::vector<Vertex> vertices;       // 모델의 정점들을 저장하는 벡터
+std::vector<Face> faces;            // 모델의 면들을 저장하는 벡터
 
-bool loadOBJ(const char* path, std::vector<Vertex>& out_vertices, std::vector<Face>& out_faces) {
+bool loadOBJ(const char* path, std::vector<Vertex>& out_vertices, std::vector<Face>& out_faces) {       // Wavefront OBJ 파일을 로드하는 함수
     std::ifstream inFile(path);
     if (!inFile.is_open()) {
         std::cerr << "Unable to open file: " << path << std::endl;
@@ -66,7 +66,7 @@ bool loadOBJ(const char* path, std::vector<Vertex>& out_vertices, std::vector<Fa
     return true;
 }
 
-Vertex crossProduct(const Vertex& a, const Vertex& b) {
+Vertex crossProduct(const Vertex& a, const Vertex& b) {         // 두 벡터의 외적을 계산하는 함수
     Vertex result;
     result.x = a.y * b.z - a.z * b.y;
     result.y = a.z * b.x - a.x * b.z;
@@ -74,7 +74,7 @@ Vertex crossProduct(const Vertex& a, const Vertex& b) {
     return result;
 }
 
-void normalize(Vertex& v) {
+void normalize(Vertex& v) {             // 벡터를 정규화하는 함수
     float length = sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
     if (length != 0.0f) {
         v.x /= length;
@@ -131,44 +131,51 @@ void display() {
     //gluLookAt(-10, 10, 15, 0, 0, 0, 0, 0, 1); // shuttle
     //gluLookAt(3, 3, 3, 0, 0, 0, 0, 0, 1); // violin_case
 
-    for (const auto& face : faces) {
-        if (face.type == TRIANGLE) {
-            glBegin(GL_TRIANGLES);
-            if (face.vertices.size() >= 3) {
-                Vertex& v1 = vertices[face.vertices[0]];
-                Vertex& v2 = vertices[face.vertices[1]];
-                Vertex& v3 = vertices[face.vertices[2]];
+    for (const auto& face : faces) {        // faces의 element(면)를 순회하면서 모든 면을 그린다.
+        if (face.type == TRIANGLE) {        // 현재 면이 삼각형인 경우,
+            glBegin(GL_TRIANGLES);          // 삼각형 그리기 시작
+            if (face.vertices.size() >= 3) {                    // 면의 정점이 3개 이상이면,
+                Vertex& v1 = vertices[face.vertices[0]];        // 면의 첫 번째 정점
+                Vertex& v2 = vertices[face.vertices[1]];        // 면의 두 번째 정점
+                Vertex& v3 = vertices[face.vertices[2]];        // 면의 세 번째 정점
 
-                Vertex vector1 = { v2.x - v1.x, v2.y - v1.y, v2.z - v1.z };
-                Vertex vector2 = { v3.x - v1.x, v3.y - v1.y, v3.z - v1.z };
-                Vertex normal = crossProduct(vector1, vector2);
-                normalize(normal);
-                glNormal3f(normal.x, normal.y, normal.z);
+                Vertex vector1 = { v2.x - v1.x, v2.y - v1.y, v2.z - v1.z };     // v1에서 v2으로 향하는 벡터를 계산
+                Vertex vector2 = { v3.x - v1.x, v3.y - v1.y, v3.z - v1.z };     // v1에서 v3으로 향하는 벡터를 계산
+                Vertex normal = crossProduct(vector1, vector2);                 // 두 벡터를 외적합니다. (법선 벡터 구하기)
+                normalize(normal);                                              // 법선 벡터를 정규화 (크기를 1로 변경)
+                glNormal3f(normal.x, normal.y, normal.z);                       // OpenGL에 정규화된 법선 벡터를 설정
             }
             for (int vertexIndex : face.vertices) {
-                glVertex3f(vertices[vertexIndex].x, vertices[vertexIndex].y, vertices[vertexIndex].z);
+                glVertex3f(vertices[vertexIndex].x, vertices[vertexIndex].y, vertices[vertexIndex].z);      // 삼각형의 세 점 그리기 (for loop)
             }
-            glEnd();
+            glEnd();        // 삼각형 그리기 종료
         }
-        else if (face.type == SQUARE) {
-            glBegin(GL_QUADS);
-            if (face.vertices.size() >= 4) {
-                Vertex& v1 = vertices[face.vertices[0]];
-                Vertex& v2 = vertices[face.vertices[1]];
-                Vertex& v3 = vertices[face.vertices[2]];
+        else if (face.type == SQUARE) {     // 현재 면이 사각형인 경우
+            glBegin(GL_QUADS);              // 사각형 그리기 시작
+            if (face.vertices.size() >= 4) {                    // 면의 정점이 4개 이상이면,
+                Vertex& v1 = vertices[face.vertices[0]];        // 면의 첫 번째 정점
+                Vertex& v2 = vertices[face.vertices[1]];        // 면의 두 번째 정점
+                Vertex& v3 = vertices[face.vertices[2]];        // 면의 세 번째 정점
 
-                Vertex vector1 = { v2.x - v1.x, v2.y - v1.y, v2.z - v1.z };
-                Vertex vector2 = { v3.x - v1.x, v3.y - v1.y, v3.z - v1.z };
-                Vertex normal = crossProduct(vector1, vector2);
-                normalize(normal);
-                glNormal3f(normal.x, normal.y, normal.z);
+                Vertex vector1 = { v2.x - v1.x, v2.y - v1.y, v2.z - v1.z };     // v1에서 v2으로 향하는 벡터를 계산
+                Vertex vector2 = { v3.x - v1.x, v3.y - v1.y, v3.z - v1.z };     // v1에서 v3으로 향하는 벡터를 계산
+                Vertex normal = crossProduct(vector1, vector2);                 // 두 벡터를 외적합니다. (법선 벡터 구하기)
+                normalize(normal);                                              // 법선 벡터를 정규화 (크기를 1로 변경)
+                glNormal3f(normal.x, normal.y, normal.z);                       // OpenGL에 정규화된 법선 벡터를 설정
             }
             for (int vertexIndex : face.vertices) {
-                glVertex3f(vertices[vertexIndex].x, vertices[vertexIndex].y, vertices[vertexIndex].z);
+                glVertex3f(vertices[vertexIndex].x, vertices[vertexIndex].y, vertices[vertexIndex].z);      // 사각형의 네 점 그리기 (for loop)
             }
-            glEnd();
+            glEnd();        // 사각형 그리기 종료
         }
     }
+    
+    /**
+    법선벡터를 설정하는 이유(glNormal3f)
+    ================================================================================
+    삼각형이나 사각형의 표면 법선 벡터를 OpenGL에 제공함으로써, 광원이 어떻게 표면에 반사되는지를 결정합니다.
+    법선 벡터가 없으면 광원 계산이 제대로 이루어지지 않아 조명이 이상하게 나타날 수 있습니다.
+     */
 
     glutSwapBuffers();
 }
