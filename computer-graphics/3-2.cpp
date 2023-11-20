7fa32fe5c2c321107b785806a58bd0a826f4e591
조성원 <tjddnjsjo@kakao.com>
2023-11-20 17:30:13 +0900

    refactor: 주석 추가

--- computer-graphics/3-2.cpp
+++ computer-graphics/3-2.cpp
@@ -2,6 +2,7 @@
 #include <GL/glut.h>
 #include <stdio.h>
 
+// 4개의 컨트롤 포인트를 정의합니다.
 float P0[2] = { -5.0f, -7.0f };
 float P1[2] = { -3.0f, 2.0f };
 float P2[2] = { 8.0f, -4.0f };
@@ -12,73 +13,77 @@ float P3[2] = { 5.0f, 7.0f };
 //float P3[2] = { 5.0f, -3.0f };
 
 void drawAxis() {
-    glBegin(GL_LINES);
-    glColor3f(1, 0, 0); glVertex2f(-10, 0); glVertex2f(10, 0);
-    glColor3f(0, 0, 1); glVertex2f(0, -10); glVertex2f(0, 10);
-    glEnd();
+    glBegin(GL_LINES);                                              // 선 그리기 시작
+    glColor3f(1, 0, 0); glVertex2f(-10, 0); glVertex2f(10, 0);      // x축 그리기
+    glColor3f(0, 0, 1); glVertex2f(0, -10); glVertex2f(0, 10);      // y축 그리기
+    glEnd();                                                        // 선 그리기 종료
 }
 
 void drawGrid() {
-    glBegin(GL_LINES);
-    glColor3f(0.4, 0.4, 0.4);
-    for (int i = -10; i <= 10; i++) {
-        if (i != 0) {
-            glVertex2f(-10, i); glVertex2f(10, i);
-            glVertex2f(i, -10); glVertex2f(i, 10);
+    glBegin(GL_LINES);                                  // 선 그리기 시작
+    glColor3f(0.4, 0.4, 0.4);                           // 회색으로 설정
+    for (int i = -10; i <= 10; i++) {                   // -10부터 10까지 반복
+        if (i != 0) {                                   // x축, y축을 제외하고 (drawAxis에서 그리므로)
+            glVertex2f(-10, i); glVertex2f(10, i);      // 수평선 그리기
+            glVertex2f(i, -10); glVertex2f(i, 10);      // 수직선 그리기
         }
     }
-    glEnd();
+    glEnd();                                            // 선 그리기 종료
 }
 
 void drawPoints() {
-    glPointSize(5.0f);
+    glPointSize(5.0f);                      // 점의 크기를 5로 설정합니다.
 
-    glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-    glBegin(GL_POINTS);
-    glVertex2f(P0[0], P0[1]);
-    glVertex2f(P1[0], P1[1]);
-    glVertex2f(P2[0], P2[1]);
-    glVertex2f(P3[0], P3[1]);
-    glEnd();
+    glColor4f(0.0f, 1.0f, 0.0f, 1.0f);      // 색상을 녹색으로 설정합니다.
+    glBegin(GL_POINTS);                     // 점 그리기 시작
+    glVertex2f(P0[0], P0[1]);               // P0 좌표에 점을 그립니다.
+    glVertex2f(P1[0], P1[1]);               // P1 좌표에 점을 그립니다.
+    glVertex2f(P2[0], P2[1]);               // P2 좌표에 점을 그립니다.
+    glVertex2f(P3[0], P3[1]);               // P3 좌표에 점을 그립니다.
+    glEnd();                                // 점 그리기 종료
 }
 
 void drawText(float x, float y, const char* string) {
-    glRasterPos2f(x, y);
+    glRasterPos2f(x, y);                                        // 출력 위치를 설정합니다.
     for (const char* c = string; *c != '\0'; c++) {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, *c);
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, *c);      // 문자열을 한 글자씩 출력합니다.
     }
 }
 
+
 void drawControlLines() {
-    glColor4f(0.0f, 0.0f, 0.0f, 1.0f);
-    glBegin(GL_LINES);
-    glVertex2f(P0[0], P0[1]);
-    glVertex2f(P1[0], P1[1]);
-    glVertex2f(P1[0], P1[1]);
-    glVertex2f(P2[0], P2[1]);
-    glVertex2f(P2[0], P2[1]);
-    glVertex2f(P3[0], P3[1]);
-    glEnd();
+    glColor4f(0.0f, 0.0f, 0.0f, 1.0f);      // 검은색으로 설정합니다.
+    glBegin(GL_LINES);                      // 선 그리기 시작
+    glVertex2f(P0[0], P0[1]);               // P0에서 시작하는 선을 그립니다.
+    glVertex2f(P1[0], P1[1]);               // P1에서 끝나는 선을 그립니다.
+    glVertex2f(P1[0], P1[1]);               // P1에서 시작하는 선을 그립니다.
+    glVertex2f(P2[0], P2[1]);               // P2에서 끝나는 선을 그립니다.
+    glVertex2f(P2[0], P2[1]);               // P2에서 시작하는 선을 그립니다.
+    glVertex2f(P3[0], P3[1]);               // P3에서 끝나는 선을 그립니다.
+    glEnd();                                // 선 그리기 종료
 }
 
 void drawBezierCurve() {
-    glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-    glBegin(GL_LINE_STRIP);
-    for (float t = 0.0f; t <= 1.0f; t += 0.01f) {
-        float x = (1 - t) * (1 - t) * (1 - t) * P0[0] + 3 * (1 - t) * (1 - t) * t * P1[0] + 3 * (1 - t) * t * t * P2[0] + t * t * t * P3[0];
-        float y = (1 - t) * (1 - t) * (1 - t) * P0[1] + 3 * (1 - t) * (1 - t) * t * P1[1] + 3 * (1 - t) * t * t * P2[1] + t * t * t * P3[1];
-        glVertex2f(x, y);
+    glColor4f(0.0f, 1.0f, 0.0f, 1.0f);                                      // 곡선의 색상을 녹색으로 설정합니다.
+    glBegin(GL_LINE_STRIP);                                                 // 선 그리기 시작
+    for (float t = 0.0f; t <= 1.0f; t += 0.01f) {                           // t값을 0부터 1까지 0.01씩 증가시키며 반복합니다.
+        float x = (1 - t) * (1 - t) * (1 - t) * P0[0] + 3 * (1 - t) * (1 - t) * t * P1[0] + 
+                   3 * (1 - t) * t * t * P2[0] + t * t * t * P3[0];         // x좌표를 계산합니다. (Assignment03.pdf 20페이지의 수식 참고)
+        float y = (1 - t) * (1 - t) * (1 - t) * P0[1] + 3 * (1 - t) * (1 - t) * t * P1[1] + 
+                   3 * (1 - t) * t * t * P2[1] + t * t * t * P3[1];         // y좌표를 계산합니다. (Assignment03.pdf 20페이지의 수식 참고)
+        glVertex2f(x, y);                                                   // 계산된 좌표를 이용하여 점을 그립니다.
     }
-    glEnd();
+    glEnd();                                                                // 선 그리기 종료
 }
 
 void display() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    drawAxis();
-    drawGrid();
+    glClear(GL_COLOR_BUFFER_BIT);   // 화면을 지웁니다.
+    drawAxis();                     // 좌표축을 그립니다.
+    drawGrid();                     // 격자를 그립니다.
 
-    drawPoints();
+    drawPoints();                   // 컨트롤 포인트를 그립니다.
 
+    // 각 point마다 라벨 생성 및 출력
     char label[20];
     sprintf(label, "P0(%.1f, %.1f)", P0[0], P0[1]);
     drawText(P0[0] + 0.2, P0[1], label);
@@ -89,28 +94,32 @@ void display() {
     sprintf(label, "P3(%.1f, %.1f)", P3[0], P3[1]);
     drawText(P3[0] + 0.2, P3[1], label);
 
-    drawControlLines();
+    drawControlLines();             // 컨트롤 라인을 그립니다.
 
-    drawBezierCurve();
+    drawBezierCurve();              // 베지어 곡선을 그립니다.
 
-    glFlush();
+    glFlush();                      // 그리기 명령을 수행합니다.
 }
 
 void init() {
-    glClearColor(0.6, 0.6, 0.6, 1.0);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(-10, 10, -10, 10);
+    glClearColor(0.6, 0.6, 0.6, 1.0);       // 배경색을 회색으로 설정합니다.
+    glMatrixMode(GL_PROJECTION);            // 투영 행렬 모드를 설정합니다.
+    glLoadIdentity();                       // 현재 행렬을 단위 행렬로 초기화합니다.
+    gluOrtho2D(-10, 10, -10, 10);           // 2D 좌표계를 설정합니다.
 }
 
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowPosition(100, 100);
-    glutInitWindowSize(900, 900);
-    glutCreateWindow("2D Grid");
-    init();
-    glutDisplayFunc(display);
-    glutMainLoop();
+    glutInit(&argc, argv);                              // GLUT 라이브러리를 초기화합니다.
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);        // 디스플레이 모드를 설정합니다.
+    glutInitWindowPosition(100, 100);                   // 윈도우의 위치를 설정합니다.
+    glutInitWindowSize(900, 900);                       // 윈도우의 크기를 설정합니다.
+    glutCreateWindow("2D Grid");                        // 윈도우를 생성합니다.
+    init();                                             // 초기화 함수를 호출합니다.
+    glutDisplayFunc(display);                           // 디스플레이 콜백 함수를 등록합니다.
+    glutMainLoop();                                     // 이벤트 루프를 시작합니다.
     return 0;
 }
+
+
+
+
--- computer-graphics/3-3.cpp
+++ computer-graphics/3-3.cpp
@@ -10,126 +10,132 @@ GLfloat ctrlpoints[4][4][3] = {
 };
 
 void drawAxis() {
-    glBegin(GL_LINES);
-    glColor3f(1, 0, 0); glVertex3f(0, 0, 0); glVertex3f(6.5, 0, 0);
-    glColor3f(0, 1, 0); glVertex3f(0, 0, 0); glVertex3f(0, 4, 0);
-    glColor3f(0, 0, 1); glVertex3f(0, 0, 0); glVertex3f(0, 0, -8);
-    glEnd();
+    glBegin(GL_LINES);                                                  // 선 그리기 시작
+    glColor3f(1, 0, 0); glVertex3f(0, 0, 0); glVertex3f(6.5, 0, 0);     // x축 그리기
+    glColor3f(0, 1, 0); glVertex3f(0, 0, 0); glVertex3f(0, 4, 0);       // y축 그리기
+    glColor3f(0, 0, 1); glVertex3f(0, 0, 0); glVertex3f(0, 0, -8);      // z축 그리기
+    glEnd();                                                            // 선 그리기 종료
 }
 
 void drawPlane() {
-    glColor3f(1.0, 1.0, 1.0);
+    glColor3f(1.0, 1.0, 1.0);                   // Matrix의 색상을 흰색으로 설정
 
-    glBegin(GL_LINES);
-    for (GLfloat i = 0; i <= 6; i += 2) {
-        glVertex3f(i, 0, -6);
-        glVertex3f(i, 0, 0);
-        glVertex3f(6, 0, -i);
-        glVertex3f(0, 0, -i);
+    glBegin(GL_LINES);                          // 선 그리기 시작
+    for (GLfloat i = 0; i <= 6; i += 2) {       // 0부터 6까지 2씩 증가하면서 반복
+        glVertex3f(i, 0, -6);                   // (i, 0, -6) 좌표로 이동하여 선 그리기 시작
+        glVertex3f(i, 0, 0);                    // (i, 0, 0) 좌표로 이동하여 선 그리기 종료
+        glVertex3f(6, 0, -i);                   // (6, 0, -i) 좌표로 이동하여 선 그리기 시작
+        glVertex3f(0, 0, -i);                   // (0, 0, -i) 좌표로 이동하여 선 그리기 종료
     }
-    glEnd();
+    glEnd();                                    // 선 그리기 종료
 }
 
 void drawControlPoints() {
-    glPointSize(7.0);
-    glColor3f(1.0, 0.0, 0.0);
-    glBegin(GL_POINTS);
+    glPointSize(7.0);                               // 점의 크기 설정
+    glColor3f(1.0, 0.0, 0.0);                       // 빨간색으로 설정
+    glBegin(GL_POINTS);                             // 점 그리기 시작
     for (int i = 0; i < 4; ++i) {
         for (int j = 0; j < 4; ++j) {
-            glVertex3fv(&ctrlpoints[i][j][0]);
+            glVertex3fv(&ctrlpoints[i][j][0]);      // 모든 컨트롤 포인트 그리기
         }
     }
-    glEnd();
+    glEnd();                                        // 점 그리기 종료
 }
 
 void drawControlLines() {
-    glColor3f(0.0, 0.0, 0.0);
-    glLineWidth(2.0);
+    glColor3f(0.0, 0.0, 0.0);                       // 검은색으로 설정
+    glLineWidth(2.0);                               // 두께를 2로 설정
 
     for (int i = 0; i < 4; ++i) {
-        glBegin(GL_LINE_STRIP);
+        glBegin(GL_LINE_STRIP);                     // 선 그리기 시작
         for (int j = 0; j < 4; ++j) {
-            glVertex3fv(&ctrlpoints[i][j][0]);
+            glVertex3fv(&ctrlpoints[i][j][0]);      // 컨트롤 포인트를 이어 선 그리기
         }
-        glEnd();
+        glEnd();                                    // 선 그리기 종료
     }
 
     for (int j = 0; j < 4; ++j) {
-        glBegin(GL_LINE_STRIP);
+        glBegin(GL_LINE_STRIP);                     // 선 그리기 시작
         for (int i = 0; i < 4; ++i) {
-            glVertex3fv(&ctrlpoints[i][j][0]);
+            glVertex3fv(&ctrlpoints[i][j][0]);      // 컨트롤 포인트를 이어 선 그리기
         }
-        glEnd();
+        glEnd();                                    // 선 그리기 종료
     }
 }
 
+// 이항 계수를 계산하는 함수
 int binomialCoeff(int n, int k) {
-    if (k == 0 || k == n)
+    if (k == 0 || k == n)                                               // k가 0이거나 k가 n일 경우 1을 반환
         return 1;
-    return binomialCoeff(n - 1, k - 1) + binomialCoeff(n - 1, k);
+    return binomialCoeff(n - 1, k - 1) + binomialCoeff(n - 1, k);       // 재귀적으로 이항 계수를 계산하여 반환
 }
 
 void drawBezierSurface() {
-    glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+    glColor4f(0.0f, 0.0f, 1.0f, 1.0f);                                                      // 파란색으로 설정
     
-    for (float u = 0.0f; u < 1.0f; u += 0.001f) {
-        glBegin(GL_LINE_STRIP);
+    for (float u = 0.0f; u < 1.0f; u += 0.001f) {                                           // u값 증가 (0.001 간격으로 선을 그려 면을 만든다.)
+        glBegin(GL_LINE_STRIP);                                                             // 선 그리기 시작
         
-        for (float v = 0.0f; v < 1.0f; v += 0.001f) {
+        for (float v = 0.0f; v < 1.0f; v += 0.001f) {                                       // v값 증가 (0.001 간격으로 선을 그려 면을 만든다.)
             float x = 0.0;
             float y = 0.0;
             float z = 0.0;
             
-            for (int j = 0; j < 4; j++) {
-                for (int k = 0; k < 4; k++) {
-                    float basisU = binomialCoeff(3, j) * pow(1 - u, 3 - j) * pow(u, j);
-                    float basisV = binomialCoeff(3, k) * pow(1 - v, 3 - k) * pow(v, k);
-                    x += basisU * basisV * ctrlpoints[j][k][0];
-                    y += basisU * basisV * ctrlpoints[j][k][1];
-                    z += basisU * basisV * ctrlpoints[j][k][2];
+            for (int j = 0; j < 4; j++) {                                                   // j값 증가
+                for (int k = 0; k < 4; k++) {                                               // k값 증가
+                    float basisU = binomialCoeff(3, j) * pow(1 - u, 3 - j) * pow(u, j);     // u에 대한 기저함수 계산
+                    float basisV = binomialCoeff(3, k) * pow(1 - v, 3 - k) * pow(v, k);     // v에 대한 기저함수 계산
+                    x += basisU * basisV * ctrlpoints[j][k][0];                             // x좌표 계산
+                    y += basisU * basisV * ctrlpoints[j][k][1];                             // y좌표 계산
+                    z += basisU * basisV * ctrlpoints[j][k][2];                             // z좌표 계산
                 }
             }
-            glVertex3f(x, y, z);
+            glVertex3f(x, y, z);                                                            // 계산된 좌표로 선 그리기
         }
-        glEnd();
+        glEnd();                                                                            // 선 그리기 종료
     }
 }
 
 void display() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glEnable(GL_DEPTH_TEST);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);         // 색상 버퍼와 깊이 버퍼 초기화
+    glEnable(GL_DEPTH_TEST);                                    // 깊이 버퍼 사용
 
-    drawAxis();
-    drawPlane();
-    drawControlPoints();
-    drawControlLines();
-    drawBezierSurface();
+    drawAxis();                                                 // 축 그리기
+    drawPlane();                                                // 바닥 Matrix 그리기
+    drawControlPoints();                                        // 제어점 그리기
+    drawControlLines();                                         // 제어선 그리기
+    drawBezierSurface();                                        // 베지어 곡면 그리기
 
-    glFlush();
+    glFlush();                                                  // 명령어를 모두 실행
 }
 
 void init() {
-    glClearColor(0.6, 0.6, 0.6, 1.0);
+    glClearColor(0.6, 0.6, 0.6, 1.0);                           // 배경색 설정
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(3, 3, 4.5, 3, -2, -20, 0, 1, 0);
+    glMatrixMode(GL_MODELVIEW);                                 // 모델뷰 매트릭스 설정
+    glLoadIdentity();                                           // 현재 매트릭스를 단위행렬로 초기화
+    gluLookAt(3, 3, 4.5, 3, -2, -20, 0, 1, 0);                  // 카메라 위치 설정
 
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(60.0, 4.0 / 3.0, 1, 100);
+    glMatrixMode(GL_PROJECTION);                                // 투영 매트릭스 설정
+    glLoadIdentity();                                           // 현재 매트릭스를 단위행렬로 초기화
+    gluPerspective(60.0, 4.0 / 3.0, 1, 100);                    // 원근 투영 설정
 
-    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_DEPTH_TEST);                                    // 깊이 버퍼 사용
 }
+
 
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowPosition(120, 80);
-    glutInitWindowSize(1280, 980);
-    glutCreateWindow("Bezier Surface");
-    init();
-    glutDisplayFunc(display);
-    glutMainLoop();
+    glutInit(&argc, argv);                                          // GLUT 초기화
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);       // 디스플레이 모드 설정
+    glutInitWindowPosition(120, 80);                                // 윈도우 위치 설정
+    glutInitWindowSize(1280, 980);                                  // 윈도우 크기 설정
+    glutCreateWindow("Bezier Surface");                             // 윈도우 생성
+    init();                                                         // 초기화 함수 호출
+    glutDisplayFunc(display);                                       // 디스플레이 콜백 함수 등록
+    glutMainLoop();                                                 // 이벤트 루프 시작
     return 0;
 }
+
+
+
+
